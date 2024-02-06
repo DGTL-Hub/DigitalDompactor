1c7f399c33d11a94ed3c7b7f37c7ef856e462e64
Svyatoslav Oleg Russkiy <russkiy1389@gmail.com>
2024-02-05 20:49:57 -0500

    compact

--- src/dbConfig/dbConfig.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connect() {
-    try {mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nextjs', {}
-} catch (error) {
-    console.log('Error connecting to database')
-    console.log(error)
-    throw error
-}
